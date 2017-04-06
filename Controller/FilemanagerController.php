0e157b36e61dc42edc403d31c2663e0420b3ff95
Trimech Mehdi <trimechmehdi11@gmail.com>
2017-04-07 00:32:04 +0100

    comments controller

--- Controller/FilemanagerController.php
+++ Controller/FilemanagerController.php
@@ -17,6 +17,10 @@ use Core\FilemanagerBundle\Entity\Files;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class FilemanagerController
+ * @package Core\FilemanagerBundle\Controller
+ */
 class FilemanagerController extends Controller
 {
     /**
@@ -25,18 +29,23 @@ class FilemanagerController extends Controller
      */
     public function indexAction(Request $request)
     {
+        /** @var get the default folder in database $manager */
         $manager = $this->get('filemanager.folder.manager');
         if ($request->query->has('dir')) {
             $folder = $manager->find($request->query->get('dir'));
         } else {
             $folder = $manager->main();
             if (!$folder) {
+                /** @var init the main root defined on the bundle configuration $folder */
                 $folder = $manager->initRootDir();;
             }
         }
 
+        /** @var get Directories from folders_table $query */
         $query = $manager->repository();
         $request->query->has('direction') ? $direction = $request->query->get('direction') : $direction = 'asc';
+
+        /** apply sort by parameter */
         if ($request->query->has('sort')) {
             switch ($request->query->get('sort')) {
                 case 'name':
@@ -55,9 +64,12 @@ class FilemanagerController extends Controller
             $directories = $query->getChildren($folder, true);
         }
 
+        /** get view (list or grid) from the request or define it as Grid */
         $request->query->has('view') ? $view = $request->query->get('view') : $view = 'grid';
 
+        /** @var File Upload $formUpload */
         $formUpload = $this->createForm('Core\FilemanagerBundle\Form\File', new Files());
+
         return $this->render('@Filemanager/layout.html.twig', array(
             'directories' => $directories,
             'folder' => $folder,
@@ -72,23 +84,31 @@ class FilemanagerController extends Controller
      */
     public function doAction(Request $request)
     {
+        /** @var Folder Manager From Container $manager */
         $manager = $this->get('filemanager.folder.manager');
 
+        /** execute actions */
         switch ($request->get('action')) {
+            /** create folder */
             case 'create':
+                /** @var get the target dir $target */
                 $target = $manager->find($request->get('target'));
 
+                /** @var folder name $path */
                 $path = $request->get('folder_name');
                 $folderName = $request->get('folder_name');
 
+                /** @var insert the folder and create it $folder */
                 $folder = $this->get('filemanager.folder.manager')->create($path, $folderName, $folderName, $target);
 
                 return $this->redirectToRoute('filemanager_index', array('dir' => $folder->getId()));
                 break;
+            /** delete folder */
             case 'delete':
                 $manager->delete($request->get('target'));
                 return $this->redirectToRoute('filemanager_index', array('dir' => $request->get('dir')));
                 break;
+            /** upload file */
             case 'upload_file':
                 $fileManager = $this->get('filemanager.file.manager');
                 $formUpload = $this->createForm('Core\FilemanagerBundle\Form\File', new Files());
